629c3facfe56c6d095cd0026e6347f0a03c46068
amongil <almonfe1@gmail.com>
2017-07-03 00:18:37 +0200

    Add webserver to the agent

--- agent/main.go
+++ agent/main.go
@@ -1,29 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/amongil/blackice/ec2utils"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/julienschmidt/httprouter"
 )
 
-func main() {
-	cl := ec2utils.NewClient()
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
 
-	input := &ec2.DescribeInstancesInput{}
-	err := cl.DescribeInstancesPages(
-		input,
-		func(p *ec2.DescribeInstancesOutput, lastPage bool) (shouldContinue bool) {
-			fmt.Println(p)
-			if p.NextToken == nil {
-				return false
-			}
-			return true
-		})
+func hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func instances(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	cl := ec2utils.NewClient("eu-central-1")
+	instances, err := cl.GetInstancesByKeyPair(ps.ByName("keyname"))
 	if err != nil {
-		aerr, _ := err.(awserr.Error)
-		fmt.Println(aerr)
-		return
+		fmt.Println(err.Error())
 	}
+	// Convert our structures to json
+	res, err := json.MarshalIndent(instances, "", "\t")
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w, string(res))
+}
+
+func main() {
+
+	router := httprouter.New()
+	router.GET("/", index)
+	router.GET("/hello/:name", hello)
+	router.GET("/instances/:keyname", instances)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
